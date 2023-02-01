bbf552b32c27e972f340650d7ea54534a5e9441c
Vihlai Maldonado <v.maldonado1@uniandes.edu.co>
2023-01-31 23:06:53 -0500

    Added improvements.

--- server/config.py
+++ server/config.py
@@ -11,7 +11,7 @@ class ApplicationConfig:
     OPENAPI_URL_PREFIX = "/"
     OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    SQLALCHEMY_DATABASE_URI = os.environ["DB_CONNECTION"]
     PROPAGATE_EXCEPTIONS = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_RUN_PORT = 5001
